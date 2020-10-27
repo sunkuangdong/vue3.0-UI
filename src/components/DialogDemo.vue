b583a01f94663317a75e05499c38b2503f38aba3
褚煦露 <chuxulu@lezhixing.com.cn>
2020-10-27 20:46:20 +0800

    基本完成官网框架

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,16 @@
+<template>
+<div>Dialog</div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style scoped lang=scss>
+</style>
---
+++ src/components/TabsDemo.vue
@@ -0,0 +1,16 @@
+<template>
+<div>Tabs</div>
+</template>
+
+<script>
+export default {
+    name: "",
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+
+<style scoped lang=scss>
+</style>

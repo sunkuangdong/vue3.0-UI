8af83c9859113271b55b80e2227f88e57f48d86e
褚煦露 <chuxulu@lezhixing.com.cn>
2020-10-28 19:42:29 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,14 +1,20 @@
 <template>
-<div>Button</div>
+  <div>Button</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import Button from "../lib/Button.vue";
 export default {
-    name: "",
-    data() {
-        return {};
-    },
-    methods: {},
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: { Button },
 };
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,19 @@
+<template>
+<button>
+    <slot/>
+</button>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped lang=scss>
+
+</style>

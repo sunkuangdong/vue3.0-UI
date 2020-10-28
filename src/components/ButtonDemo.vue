1b1df2151829f069bfbbf98baad1223d4a61b055
褚煦露 <chuxulu@lezhixing.com.cn>
2020-10-28 21:16:33 +0800

    CSS最小影响原则

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,18 +2,26 @@
   <div>Button</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button>默认</Button>
+    <Button theme="link">link</Button>
+    <Button theme="text">text</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
-  name: "",
-  data() {
-    return {};
+  setup(props) {
+    const onClick = () => {
+      console.log("onClick");
+    };
+    const onFocus = () => {
+      console.log("onFocus");
+    };
+    return {
+      onClick,
+    };
   },
-  methods: {},
   components: { Button },
 };
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,75 +1,75 @@
 <template>
-<button @click="toggle" :class="{checked:value}">
+  <button class="gulu-switch" @click="toggle" :class="{'gulu-checked':value}">
     <span></span>
-</button>
+  </button>
 </template>
 
 <script lang="ts">
-import {
-    ref
-} from "vue";
+import { ref } from "vue";
 export default {
-    props: {
-        value: Boolean,
-    },
-    setup(props, context) {
-        const toggle = () => {
-            // context.emit("input", !props.value);
-            context.emit("update:value", !props.value);
-        };
-        return {
-            toggle,
-        };
-    },
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      // context.emit("input", !props.value);
+      context.emit("update:value", !props.value);
+    };
+    return {
+      toggle,
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
 
-button {
-    height: $h;
-    width: $h * 2;
-    border: none;
-    background: #bfbfbf;
-    border-radius: $h/2;
-    position: relative;
+.gulu-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+  z-index: 0;
 
-    >span {
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2 / 2;
-        transition: all 250ms;
+  > span {
+    position: absolute;
+    z-index: 0;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+
+  &.gulu-checked {
+    background: #1890ff;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px);
     }
+  }
 
-    &.checked {
-        background: #1890ff;
+  &:focus {
+    outline: none;
+  }
 
-        >span {
-            left: calc(100% - #{$h2} - 2px);
-        }
+  &:active {
+    > span {
+      width: $h2 + 4px;
     }
+  }
 
-    &:focus {
-        outline: none;
+  &.gulu-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
     }
-
-    &:active {
-        >span {
-            width: $h2 + 4px;
-        }
-    }
-
-    &.checked:active {
-        >span {
-            width: $h2 + 4px;
-            margin-left: -4px;
-        }
-    }
+  }
 }
 </style>

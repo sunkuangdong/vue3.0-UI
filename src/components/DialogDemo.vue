a3c931c713f56d6409281e90ba559fef2720f202
褚煦露 <chuxulu@lezhixing.com.cn>
2020-10-29 20:13:02 +0800

    Dialog 支持自定义 title 和 content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,7 +3,16 @@
     <div>Dialog 示例</div>
     <h1>示例1</h1>
     <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="dialogVisible" :closeOnClickOverlay="false" :ok="ok" :cancel="cancel" />
+    <Dialog
+      v-model:visible="dialogVisible"
+      :closeOnClickOverlay="false"
+      :ok="ok"
+      :cancel="cancel"
+      :title="title"
+    >
+      <div>你好</div>
+      <div>hi</div>
+    </Dialog>
   </div>
 </template>
 
@@ -13,6 +22,7 @@ import Dialog from "../lib/Dialog.vue";
 export default {
   components: { Dialog },
   setup() {
+    const title = "你好";
     const dialogVisible = ref(false);
     const toggle = () => {
       dialogVisible.value = !dialogVisible.value;
@@ -23,7 +33,7 @@ export default {
     const cancel = () => {
       dialogVisible.value = false;
     };
-    return { dialogVisible, toggle, ok, cancel };
+    return { dialogVisible, toggle, ok, cancel, title };
   },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="gulu-dialog-wrapper">
       <div class="gulu-dialog">
         <header>
-          标题
+          {{title}}
           <span class="gulu-dialog-close" @click="close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot />
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
@@ -39,6 +38,10 @@ export default {
     cancel: {
       type: Function,
       default: () => {},
+    },
+    title: {
+      type: String,
+      default: "提示",
     },
   },
   components: { Button },

55ebb91a1056a861ff5966d982c746d3ea33327b
褚煦露 <chuxulu@lezhixing.com.cn>
2020-11-01 13:13:56 +0800

    背景色渐变

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,54 @@
 <template>
-<div>
+  <div class="topnavAndBanner">
     <Topnav />
     <div class="banner">
-        <h1>Vue3.0 UI</h1>
-        <h2>一个很厉害的 UI 框架</h2>
-        <p class="actions">
-            <a href>GitHub</a>
-            <router-link to="/doc">开始</router-link>
-        </p>
+      <h1>Vue3.0 UI</h1>
+      <h2>一个很厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href>GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
-</div>
+  </div>
 </template>
 
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 export default {
-    name: "",
-    data() {
-        return {};
-    },
-    methods: {},
-    components: {
-        Topnav,
-    },
+  components: {
+    Topnav,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+}
 .banner {
-    padding: 100px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background: lightgreen;
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-    >.actions {
-        padding: 8px 0;
+  > .actions {
+    padding: 8px 0;
 
-        a {
-            margin: 0 8px;
-            background: #fff;
-            display: inline-block;
-            $h: 28px;
-            height: $h;
-            line-height: $h;
-            border-radius: $h/2;
-            padding: 0 8px;
-        }
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
     }
+  }
 }
 </style>

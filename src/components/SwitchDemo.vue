20e9af0cb675fccb7eaa6a2cc1aeba4fc5d1efc7
褚煦露 <chuxulu@lezhixing.com.cn>
2020-11-03 21:53:44 +0800

    更新 SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <!-- <Switch :value="y" @input="y = $event" /> -->
-    <Switch v-model:value="SwitchFlag" />
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="SwitchFlag" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
+import Button from "../lib/Button.vue";
 export default {
   components: {
     Switch,
+    Button,
   },
   setup() {
     const SwitchFlag = ref(false);
@@ -21,5 +34,31 @@ export default {
 };
 </script>
 
-<style scoped lang=scss>
+<style scoped lang="scss">
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
 </style>

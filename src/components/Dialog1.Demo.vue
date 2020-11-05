efa9b2daab1b8513a3bb22b9f82f2f7ef9225b7c
褚煦露 <chuxulu@lezhixing.com.cn>
2020-11-05 20:31:52 +0800

    改造 Dialog demo

---
+++ src/components/Dialog1.Demo.vue
@@ -0,0 +1,44 @@
+<demo>
+常规用法
+</demo>
+<template>
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      v-model:visible="dialogVisible"
+      :closeOnClickOverlay="false"
+      :ok="ok"
+      :cancel="cancel"
+      :title="title"
+    >
+      <div>你好</div>
+      <div>hi</div>
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const title = "你好";
+    const dialogVisible = ref(false);
+    const toggle = () => {
+      dialogVisible.value = !dialogVisible.value;
+    };
+    const ok = () => {
+      dialogVisible.value = false;
+    };
+    const cancel = () => {
+      dialogVisible.value = false;
+    };
+    return { dialogVisible, toggle, ok, cancel, title };
+  },
+};
+</script>
--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,39 +1,18 @@
 <template>
   <div>
-    <div>Dialog 示例</div>
-    <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog
-      v-model:visible="dialogVisible"
-      :closeOnClickOverlay="false"
-      :ok="ok"
-      :cancel="cancel"
-      :title="title"
-    >
-      <div>你好</div>
-      <div>hi</div>
-    </Dialog>
+    <div>Button 示例</div>
+    <Demo :component="Dialog1Demo" />
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import Dialog from "../lib/Dialog.vue";
+import Demo from "./Demo.vue";
+import Dialog1Demo from "./Dialog1.Demo.vue";
 export default {
-  components: { Dialog },
+  components: { Demo },
   setup() {
-    const title = "你好";
-    const dialogVisible = ref(false);
-    const toggle = () => {
-      dialogVisible.value = !dialogVisible.value;
-    };
-    const ok = () => {
-      dialogVisible.value = false;
-    };
-    const cancel = () => {
-      dialogVisible.value = false;
-    };
-    return { dialogVisible, toggle, ok, cancel, title };
+    return { Dialog1Demo };
   },
 };
 </script>

94164da8644f511f0324d44e70c2ffb12caecfea
褚煦露 <chuxulu@lezhixing.com.cn>
2020-11-05 20:35:47 +0800

    改造 Tabs demo

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
-    <div>Tabs 示例</div>
-    <h1>示例1</h1>
-    <Tabs v-model:selected="selectText">
-      <Tab :title="'导航1'">内容1</Tab>
-      <Tab :title="'导航2'">内容2</Tab>
-    </Tabs>
+    <Demo :component="Tabs1Demo" />
   </div>
 </template>
 
 <script>
-import Tabs from "../lib/Tabs.vue";
-import Tab from "../lib/Tab.vue";
+import Demo from "./Demo.vue";
+import Tabs1Demo from "./Tabs1.Demo.vue";
 import { ref } from "vue";
 export default {
-  components: { Tabs, Tab },
+  components: { Demo },
   setup() {
-    const selectText = ref("导航1");
     return {
-      selectText,
+      Tabs1Demo,
     };
   },
 };

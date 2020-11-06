a8eb38b54d826d6c50699bde9eb4089736e2e1e7
孙匡东 <769296817@qq.com>
2020-11-06 22:12:59 +0800

    '一键部署'

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-<router-view />
+  <router-view />
 </template>
 
 <script>
-import {
-    provide,
-    ref
-} from "vue";
-import {
-    router
-} from "./router";
+import { provide, ref } from "vue";
+import { router } from "./router";
 
 export default {
-    name: "App",
-    setup() {
-        const width = document.documentElement.clientWidth;
-        const menuVisible = ref(width <= 500 ? false : true);
-        provide("meneVisible", menuVisible);
-        router.afterEach(() => {
-            if (width <= 500) {
-                menuVisible.value = false;
-            }
-        });
-    },
+  name: "App",
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+    provide("meneVisible", menuVisible);
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
+  },
 };
 </script>
